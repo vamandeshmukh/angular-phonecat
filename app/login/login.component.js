2c953270f835c0dc9db3383d14dbdd815b415289
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-04 18:53:23 +0530

    AngularJS

--- app/login/login.component.js
+++ app/login/login.component.js
@@ -5,12 +5,15 @@ angular.
     module('login').
     component('login', {
         templateUrl: 'login/login.template.html',
-        controller: function LoginController() {
-            this.message = "Welcome to login page!";
-            this.credentials = {
+        controller: function LoginController($scope) {
+            $scope.message = "Welcome to login page!";
+            $scope.credentials = {
                 userName: '',
                 password: '',
                 confirmPassword: ''
+            };
+            $scope.login = () => {
+                alert(`login successful for ${$scope.credentials.userName}!`);
             }
 
         }
