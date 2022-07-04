77d6c291c1ae33553f55abe6102773d61dcb2109
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-04 17:45:31 +0530

    AngularJS

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -6,19 +6,42 @@ angular.
     module('phoneList').
     component('phoneList', {
         templateUrl: 'phone-list/phone-list.template.html',
-        // template: "<p>some {{ 10 + 10 }} html<p>",
-        controller: function PhoneListController() {
-            this.phones = [
-                {
-                    name: 'Nexus S',
-                    snippet: 'Fast just got faster with Nexus S.'
-                }, {
-                    name: 'Motorola XOOM™ with Wi-Fi',
-                    snippet: 'The Next, Next Generation tablet.'
-                }, {
-                    name: 'MOTOROLA XOOM™',
-                    snippet: 'The Next, Next Generation tablet.'
-                }
-            ];
+        controller: function PhoneListController($http) {
+            var self = this;
+            self.orderProp = 'age';
+
+            // syntax 
+            // $http.get('reqtest url') .then( 'process the response') .catch('error');
+
+            $http.get('phones/phones.json').then(function (response) {
+                // console.log(response.data);
+                self.phones = response.data;
+            });
+            // $http.
         }
     });
+
+
+
+// 'use strict';
+// // Register `phoneList` component, along with its associated controller and template
+// angular.
+//     module('phoneList').
+//     component('phoneList', {
+//         templateUrl: 'phone-list/phone-list.template.html',
+//         // template: "<p>some {{ 10 + 10 }} html<p>",
+//         controller: function PhoneListController() {
+//             this.phones = [
+//                 {
+//                     name: 'Nexus S',
+//                     snippet: 'Fast just got faster with Nexus S.'
+//                 }, {
+//                     name: 'Motorola XOOM™ with Wi-Fi',
+//                     snippet: 'The Next, Next Generation tablet.'
+//                 }, {
+//                     name: 'MOTOROLA XOOM™',
+//                     snippet: 'The Next, Next Generation tablet.'
+//                 }
+//             ];
+//         }
+//     });
