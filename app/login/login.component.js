f7a8e56432a20d57c77d39d84e92bad81844df94
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-05 17:16:11 +0530

    routing

--- app/login/login.component.js
+++ app/login/login.component.js
@@ -8,18 +8,40 @@ angular.
         controller: function LoginController($scope, $http) {
 
             $scope.credentials = {
+                userId: '',
                 userName: '',
                 password: ''
             };
 
             $scope.login = () => {
-                $http.post('https://jsonplaceholder.typicode.com/users', $scope.credentials).
-                    then(
+                $http.get(`https://jsonplaceholder.typicode.com/users/${$scope.credentials.userId}`)
+                    .then(
                         (response) => {
-                        console.log(response.data);
-                        alert(`login successful for ${response.data.userName}!`);
-                    }
+                            console.log(response.data.id);
+                            console.log($scope.credentials.userId);
+                            if (response.data.id == $scope.credentials.userId) {
+                                alert(`Login successful for ${response.data.username}!`);
+                                $scope.credentials = {
+                                    userId: '',
+                                    userName: '',
+                                    password: ''
+                                };
+                                window.location.href = "/#!/home";
+                            }
+                        },
+                        (error) => {
+                            console.log(error);
+                            alert(`Wrong credentials!`);
+                            $scope.credentials = {
+                                userId: '',
+                                userName: '',
+                                password: ''
+                            };
+                            window.location.href = "/#!/login";
+                        }
+
                     );
+
             }
         }
     });
--- app/register/register.component.js
+++ app/register/register.component.js
@@ -5,8 +5,30 @@ angular.
     module('register').
     component('register', {
         templateUrl: 'register/register.template.html',
-        controller: function RegisterController($scope) {
-            $scope.message = "Welcome to register!";
+        controller: function RegisterController($scope, $http) {
+
+            $scope.credentials = {
+                userId: '',
+                userName: '',
+                password: ''
+            };
+
+            $scope.register = () => {
+                $http.post('https://jsonplaceholder.typicode.com/users', $scope.credentials).
+                    then(
+                        (response) => {
+                            console.log(response.data);
+                            alert(`Registration successful for ${response.data.userName}!`);
+                            $scope.credentials = {
+                                userId: '',
+                                userName: '',
+                                password: ''
+                            };
+                            window.location.href = "/#!/login";
+
+                        }
+                    );
+            }
         }
     });
 
